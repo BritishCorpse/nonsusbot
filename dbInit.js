2321259ad94543b676dc6b1b13c2be80c19732b5
BritishCorpse <BritishCorpse@users.noreply.github.com>
2021-12-23 18:37:51 +0200

    Added resetmoney command

--- dbInit.js
+++ dbInit.js
@@ -17,13 +17,14 @@ sequelize.sync({ force }).then(async () => {
 	const shop = [
 		CurrencyShop.upsert({ name: 'Cookie', cost: 1 }),
 		CurrencyShop.upsert({ name: 'Premium Cookie', cost: 100 }),
+		CurrencyShop.upsert({ name: 'Coke E Cola', cost: 3}),
 		CurrencyShop.upsert({ name: 'Bible', cost: 20 }),
 		CurrencyShop.upsert({ name: "Moses' Wand", cost: 500 }),
 		CurrencyShop.upsert({ name: 'Demon', cost: 666}),
 		CurrencyShop.upsert({ name: 'Lost Soul', cost: 10000}),
-		CurrencyShop.upsert({ name: 'Woman', cost: 30000000}),
-		CurrencyShop.upsert({ name: 'Man', cost: 30000000}),
-		CurrencyShop.upsert({ name: 'Doritoes', cost: 7000})
+		CurrencyShop.upsert({ name: 'Doritoes', cost: 7000}),
+		CurrencyShop.upsert({ name: 'Rank Upgrade', cost: 1000000}),
+		CurrencyShop.upsert({ name: 'Tendies', cost: 5000})
 	];
 	await Promise.all(shop);
 	console.log('Database synced');
