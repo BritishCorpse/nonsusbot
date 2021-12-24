fbca33e1b0e82fac5dea20dceedcfa58eb790599
pokergamer <pokergamer@users.noreply.github.com>
2021-12-24 15:34:50 +0300

    Add check for vip role

--- command_list/vip.js
+++ command_list/vip.js
@@ -8,6 +8,18 @@ module.exports = {
     category: 'currency',
     description: 'Join the V.I.P group for a low price of 10 million coins!',
     async execute(message, args){
+        const prefix = message.client.serverConfig.get(message.guild.id).prefix;
+
+        // Check if there is a vip role
+        const vipRoleId = message.client.serverConfig.get(message.guild.id).vip_role_id;
+        const vipRole = await message.guild.roles.cache.get(vipRoleId);
+
+        if (vipRole === undefined) {
+            message.channel.send(`There is no VIP role for this server. See ${prefix}config.`);
+            return;
+        }
+
+        // Check if the user has a vip pass
         const item = await CurrencyShop.findOne({
             where: {
                 name: {
@@ -22,18 +34,16 @@ module.exports = {
             }
         });
 
-
         const userItems = await user.getItems();
         for (const userItem of userItems) {
             const userVIP = userItems.find(userItem => userItem.item_id == item.id);
 
             if (userVIP === undefined) {
-                const prefix = message.client.serverConfig.get(message.guild.id).prefix;
                 message.channel.send(`You do not have the VIP pass. See the ${prefix}shop to buy it.`);
             } else {
                 message.channel.send("It appears you have the VIP pass. Welcome to the VIP Group!");
 
-                const vipRole = await message.guild.roles.cache.get(message.client.serverConfig.get(message.guild.id).vip_role_id);
+                // Give vip role
                 message.member.roles.add(vipRole);
             }
         };
