413e4a877965a8d7926a546bd1072ce84dd500ae
BritishCorpse <BritishCorpse@users.noreply.github.com>
2021-12-24 23:34:09 +0200

    Changed givemoney command, major changes.

--- command_list/givemoney.js
+++ command_list/givemoney.js
@@ -3,6 +3,9 @@ module.exports = {
     category: "Currency",
     description: "Get free money (for testing).",
     execute (message, args) {
+        if (message.author.id !== '484644637420552202' && message.author.id !== '907114729707806740') {
+            return message.channel.send("You are not the correct person")
+        }
         message.client.currency.add(message.author.id, 2000);
         message.channel.send("You got 2000💰");
     }
