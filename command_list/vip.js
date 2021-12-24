b6ef42bfd48c548c6f899482e04592cdf5ab902d
BritishCorpse <BritishCorpse@users.noreply.github.com>
2021-12-24 12:39:01 +0200

    yes

--- command_list/vip.js
+++ command_list/vip.js
@@ -14,42 +14,29 @@ module.exports = {
             }
         });
 
-<<<<<<< HEAD
 
   
         const user = await Users.findOne({
             where: {
                 user_id: message.author.id
-=======
-        const user = await Users.findOne({
-            where: {
-                user_id: message.member.id
->>>>>>> fa8a8a62d1a2a8f8566776abeb540704dd1edc9d
             }
         });
 
+
         const userItems = await user.getItems();
         for (const userItem of userItems) {
-            const userVIP = userItems.find(userItem => userItem.item_id == item.id);
+            const userVIP = userItems.find(userItem => userItem.name === item.name);
 
-<<<<<<< HEAD
             if (userVIP !== undefined) {
                 message.channel.send("It appears you have the VIP pass. Welcome to the VIP Group!");
 
                 let role = message.member.guild.roles.cache.find(role => role.name === "VIP");
                 if (role) message.guild.members.cache.get(message.author.id).roles.add(role);
-=======
-            if (userVIP === undefined) {
-                const prefix = message.client.serverConfig.get(message.guild.id).prefix;
-                message.channel.send(`You do not have the VIP pass. See the ${prefix}shop to buy it.`);
-            } else {
-                message.channel.send("It appears you have the VIP pass. Welcome to the VIP Group!");
-
-                const vipRole = await message.guild.roles.cache.get(message.client.serverConfig.get(message.guild.id).vip_role_id);
-                message.member.roles.add(vipRole);
->>>>>>> fa8a8a62d1a2a8f8566776abeb540704dd1edc9d
             }
 
+            else {
+                return message.channel.send("It appears you do not possess the VIP pass. Please go to the _shop to buy it.");
+            }
         };
 
     }
