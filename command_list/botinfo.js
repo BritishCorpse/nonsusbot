73e0f153ca5a250246569d96c3d46ca80272764a
BritishCorpse <BritishCorpse@users.noreply.github.com>
2021-12-25 00:19:19 +0200

    yes

--- command_list/botinfo.js
+++ command_list/botinfo.js
@@ -13,7 +13,8 @@ module.exports = {
           .addFields(
               {name: 'Creators', value: '<@!484644637420552202> (and <@!786301097953591326>)'},
               {name: 'Main library', value: 'discord.js'}
-          );
+          )
+          .setDescription("Graveyard is a powerful bot created to replace many multipurpose bot. There is no premium version of Graveyard, everything is free!");
 
       message.channel.send({embeds: [embed]});
     }
