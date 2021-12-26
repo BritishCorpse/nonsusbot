9f46e3d164a39e980db3505f502d289f196e6e37
pokergamer <pokergamer@noreply.users.github.com>
2021-12-26 13:21:52 +0300

    Match regex time

--- command_list/tool/reminder.js
+++ command_list/tool/reminder.js
@@ -7,5 +7,11 @@ module.exports = {
     description: "Remind you about something. Usage: <time> <message>",
     execute (message, args) {
         const timeString = args[0];
+
+        const timeRegex = /\d+d\d+h\d+m\d+s/;
+
+        const match = timeString.match(timeRegex);
+
+        message.channel.send(JSON.stringify(match));
     }
 }
