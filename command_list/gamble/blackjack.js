6f645dabada2f5d1b70554d9991eca0928d10d0a
pokergamer <pokergamer@noreply.users.github.com>
2021-12-27 11:31:54 +0300

    Fix blackjack rules and add botinfo servers;2C

--- command_list/gamble/blackjack.js
+++ command_list/gamble/blackjack.js
@@ -90,7 +90,18 @@ module.exports = {
 
         const userBet = Number.parseInt(args[0]);
 
-        if (args[0] === undefined || args[0] === '') {
+        if (args[0] === 'rules') {
+            const embed = new MessageEmbed()
+            .setTitle("Rules of blackjack.")
+            .setColor("ORANGE")
+            .setDescription(
+                `Each participant attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21.\nThe player to the left goes first and must decide whether to "stand" (not ask for another card) or "hit" (ask for another card in an attempt to get closer to a count of 21, or even hit 21 exactly). Thus, a player may stand on the two cards originally dealt to them, or they may ask the dealer for additional cards, one at a time, until deciding to stand on the total (if it is 21 or under), or goes "bust" (if it is over 21). In the latter case, the player loses and the dealer collects the bet wagered. The dealer then turns to the next player to their left and serves them in the same manner.\nWhen the dealer has served every player, the dealers face-down card is turned up. If the total is 17 or more, it must stand. If the total is 16 or under, they must take a card. The dealer must continue to take cards until the total is 17 or more, at which point the dealer must stand. If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer must count the ace as 11 and stand. The dealer's decisions, then, are automatic on all plays, whereas the player always has the option of taking one or more cards.\nThe maximum bet for this gamemode is 25 million 💰's.`
+            )
+            .addField('Rules credit', "[Bicycle Cards](https://bicyclecards.com/how-to-play/blackjack/)");
+
+            message.channel.send({embeds: [embed]});
+            return;
+        } else if (args[0] === undefined || args[0] === '') {
             message.channel.send(`🎲You did not specify your bet! Usage: ${prefix}blackjack <bet>🎲`);
             return;
         } else if (userBet <= 0 || userBet.toString() === 'NaN') { // invalid bets
@@ -99,19 +110,7 @@ module.exports = {
         } else if (userBet > 25000000) {
             message.channel.send("🎲Unfortunately your bet is too large for this game. We can't have you being too successful after all!🎲");
             return;
-        } else if (args[0] === 'rules') {
-            const embed = new MessageEmbed()
-            .setTitle("Rules of blackjack.")
-            .setColor("ORANGE")
-            .setDescription(
-                `Each participant attempts to beat the dealer by getting a count as close to 21 as possible, without going over 21.\nThe player to the left goes first and must decide whether to "stand" (not ask for another card) or "hit" (ask for another card in an attempt to get closer to a count of 21, or even hit 21 exactly). Thus, a player may stand on the two cards originally dealt to them, or they may ask the dealer for additional cards, one at a time, until deciding to stand on the total (if it is 21 or under), or goes "bust" (if it is over 21). In the latter case, the player loses and the dealer collects the bet wagered. The dealer then turns to the next player to their left and serves them in the same manner.\nWhen the dealer has served every player, the dealers face-down card is turned up. If the total is 17 or more, it must stand. If the total is 16 or under, they must take a card. The dealer must continue to take cards until the total is 17 or more, at which point the dealer must stand. If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer must count the ace as 11 and stand. The dealer's decisions, then, are automatic on all plays, whereas the player always has the option of taking one or more cards.\nThe maximum bet for this gamemode is 25 million 💰's`
-            )
-            .setFooter("Rule credit: https://bicyclecards.com/how-to-play/blackjack/")
-
-            message.channel.send({embeds: [embed]});
-            return;
-        };
-
+        }
         
         const usedCardIdentifiers = []; // keeps track of all cards taken so that they can't be taken anymore
         function getCard() {
--- command_list/general/botinfo.js
+++ command_list/general/botinfo.js
@@ -10,9 +10,10 @@ module.exports = {
       const embed = new MessageEmbed()
           .setColor("BLUE")
           .setTitle(bot_name)
+          .setDescription("Graveyard is a powerful bot created to replace many multipurpose bot. There is no premium version of Graveyard, everything is free!")
           .addField('Creators', `<@!${developer_discord_user_ids.join("> <@!")}>`)
           .addField('Main library', '[discord.js](https://discord.js.org/)')
-          .setDescription("Graveyard is a powerful bot created to replace many multipurpose bot. There is no premium version of Graveyard, everything is free!");
+          .addField('Servers', `${message.client.guilds.cache.size}`);
 
       message.channel.send({embeds: [embed]});
     }
