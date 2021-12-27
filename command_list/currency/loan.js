1b7e34a0b93a72b16321ba21cae680cb6ea9d029
BritishCorpse <BritishCorpse@users.noreply.github.com>
2021-12-28 01:13:25 +0200

    Added pseucode for loan.js, disabled messageLogging.

---
+++ command_list/currency/loan.js
@@ -0,0 +1,6 @@
+// user defines how much loan they want up to a certain point, then user gets said money
+// loan payback has an interest, meaning the amount they have to pay back per day that the loan is not paid back is increased.
+// interest is not yet agreed upon, most likely 10%
+// if user does not pay back loan within 30 days, the user will forcibly be removed 1.5 times the sum of the total loan + interest.
+
+//ILL DO THIS LATER KTHX
