f101cef4ba1f59e3f851bc6ed9c583a0ee58bb9f
pokergamer <pokergamer@noreply.users.github.com>
2021-12-28 11:09:44 +0300

    Disable levelup up because it crashes

--- background_tasks/levelup.js
+++ background_tasks/levelup.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: 'levelup',
     execute(client) {
-        client.on('messageCreate', message => {
+        // crashes because channel not in dev serv i think so i disabled
+        /*client.on('messageCreate', message => {
             const channel = client.channels.cache.get("869030517419417630");
 
             if (message.guild === null || message.author.bot) return;
@@ -9,7 +10,6 @@ module.exports = {
             if (message.channel.id !== channel.id) return console.log("no");
 
             message.channel.send("Good job! I'm proud.")
-        })
-
+        })*/
     }
 }
