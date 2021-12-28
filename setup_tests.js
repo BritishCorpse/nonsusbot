00a78437ffc5a489fa938feb1005f7fa66657856
pokergamer <pokergamer@noreply.users.github.com>
2021-12-28 16:56:39 +0300

    Update tests

--- setup_tests.js
+++ setup_tests.js
@@ -1,8 +1,6 @@
-// inspired by https://github.com/IanMitchell/jest-discord
-
 const Discord = require("discord.js");
 
-const developmentConfig = require('./development_config.json');
+const developmentConfig = require("./development_config.json");
 
 
 const client = new Discord.Client({
@@ -15,8 +13,21 @@ const client = new Discord.Client({
     ]
 });
 
+// Create custom functions accessible from tests to send commands and get response
 client.prompt = (channel, message, timeout=10000) => {
-    
+    return new Promise(resolve => {
+        channel.send(message)
+        .then(() => {
+            const filter = m => m;
+            channel.awaitMessages({
+                filter,
+                max: 1,
+                time: timeout,
+                errors: ['time']
+            })
+            .then(messages => resolve(messages.first()));
+        });
+    });
 };
 
 client.login(developmentConfig.testing_bot_token);
@@ -25,9 +36,8 @@ client.once("ready", async () => {
     client.user.setActivity("testing the Graveyard bot");
     console.log("Testing bot ready and logged in as " + client.user.tag + "!");
     console.log("\u0007"); // bell sound
-
-    const guild = client.guilds.cache.get(developmentConfig.testing_discord_server_id);
-    console.log(guild);
 });
 
-
+module.exports = {
+    client
+};
