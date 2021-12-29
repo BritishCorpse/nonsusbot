61417e3176f1485a74ec5400cefe85ed93ee6c34
pokergamer <pokergamer@noreply.users.github.com>
2021-12-29 19:13:21 +0300

    Create usagetest.js

---
+++ command_list/dummy/usagetest.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: 'buttontest',
+    description: "test buttons",
+    developer: true,
+    usage: {
+        
+    }
+    execute (message, args) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('primary')
+                    .setLabel('Primary')
+                    .setStyle('DANGER')
+            );
+
+        message.channel.send({content: 'test', components: [row]})
+        .then(botMessage => {
+            const collector = botMessage.createMessageComponentCollector({componentType: 'BUTTON', time: 30000})
+
+            collector.on("collect", interaction => {
+                interaction.deferUpdate();
+                console.log(interaction);
+            });
+
+            collector.on("end", collected => {
+                message.channel.send('timed out');
+
+                for (const button of row.components) {
+                    button.setDisabled(true);
+                }
+                botMessage.edit({components: [row]});
+
+                console.log(row);
+            });
+        });
+    }
+}
