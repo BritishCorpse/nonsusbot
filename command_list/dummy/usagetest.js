1b9bd6fb3a7bced2d176cea71f7ebef9e59e9b30
pokergamer <pokergamer@noreply.users.github.com>
2021-12-30 10:22:34 +0300

    Finish usage tests

--- command_list/dummy/usagetest.js
+++ command_list/dummy/usagetest.js
@@ -2,55 +2,44 @@ module.exports = {
     name: 'usagetest',
     description: "test usage checking",
     developer: true,
+
     usage: [    // used both for documentation in help and for actually checking the usage of a command before running it
         {
-            tag: "firstarg" // the <tag> it shows in the help command, for example _config <set> <channelid>
-            example: "firstarg", // optional, for more help ?
-            check: { // the test(s) the argument must pass to be valid; also the info the help command will show for each argument
-                is: "firstarg",             // checks if argument is exactly this
-                // isinteger: true,         // checks if argument is a number
+            tag: "nothing", // the thing it shows in usage
+            checks: {
+                isempty: null, // when you want no argument
+            },
+        },
+        {
+            tag: "firstarg",
+            example: "345", // for more help (optional)
+            checks: {            // the test(s) the argument must pass to be valid; also the info the help command will show for each argument
+                is: {not: "wasd"},          // checks if argument is exactly this (not inverts the check, so in this case cannot be "wasd")
+                isinteger: null,            // checks if argument is a number
                 // matches: /regex/,        // checks if argument matches the regex
-                // custom: (arg) => {}      // custom function for more complex logic if needed, but shouldn't be needed
+                passes: {                // custom function for more complex logic if needed, but shouldn't be needed
+                     func: arg => arg === "123",
+                     description: (not, value) => `is ${not ? "not " : ""}123`, // custom description in the usage
+                },
             },
             next: [       // next argument when first arg passes the test
                 {
-                    
+                    tag: "secondarg",
+                    checks: {
+                        is: "second",
+                    },
                 },
                 {
-                    
+                    tag: "secondargv2",
+                    checks: {
+                        is: "secondv2",
+                    },
                 },
             ],
         },
     ],
 
     execute (message, args) {
-        const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('primary')
-                    .setLabel('Primary')
-                    .setStyle('DANGER')
-            );
-
-        message.channel.send({content: 'test', components: [row]})
-        .then(botMessage => {
-            const collector = botMessage.createMessageComponentCollector({componentType: 'BUTTON', time: 30000})
-
-            collector.on("collect", interaction => {
-                interaction.deferUpdate();
-                console.log(interaction);
-            });
-
-            collector.on("end", collected => {
-                message.channel.send('timed out');
-
-                for (const button of row.components) {
-                    button.setDisabled(true);
-                }
-                botMessage.edit({components: [row]});
-
-                console.log(row);
-            });
-        });
+        message.reply("i worked yay");
     }
 }
