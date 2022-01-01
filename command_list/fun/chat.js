e32020c0d68f123a8f8ea16db91338425a2179a0
pokergamer <pokergamer@noreply.users.github.com>
2022-01-01 18:42:26 +0300

    Remove useless if statement now that bot doesn't ping people

--- command_list/fun/chat.js
+++ command_list/fun/chat.js
@@ -13,10 +13,11 @@ module.exports = {
     ],
 
     execute (message, args) {
-        if (/@/m.test(args.join(" "))) {
-            message.channel.send("No no nooo, very baaad! :poop:");
-            return;
-        }
+        // Check below no longer needed
+        //if (/@/m.test(args.join(" "))) {
+        //    message.channel.send("No no nooo, very baaad! :poop:");
+        //    return;
+        //}
 
         request("https://api.affiliateplus.xyz/api/chatbot?message=" + args.join(" ") + "&botname=kekbot&ownername=kekbot_owner&user=" + message.channel.id, (error, response, body) => {
             const parsedBody = JSON.parse(body);
