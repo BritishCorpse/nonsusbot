f9a920397b64b63d3b16fc55a4b34d6815355e04
pokergamer <pokergamer@noreply.users.github.com>
2022-01-09 16:33:08 +0300

    Make leaderboard be in order

--- command_list/currency/leaderboard.js
+++ command_list/currency/leaderboard.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 const { Users } = require(`${__basedir}/db_objects`);
 
 module.exports = {
-    name: "leaderboard",
+    name: ["leaderboard", "lb"],
     description: "Displays the richest users on the leaderboard.",
 
     usage: [
@@ -11,38 +11,29 @@ module.exports = {
     async execute(message) {
         const randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-        async function defineUser(userId) {
-            const userInDb = await Users.findOne({ where: { user_id: userId } });
-            return userInDb;
+        function defineUser(userId) {
+            return Users.findOne({ where: {user_id: userId} });
         }
 
         const embed = new MessageEmbed()
             .setTitle("Top 10 Richest People Anywhere")
             .setColor(randomColor);
 
-        async function sendEmbed() {
-            message.channel.send({ embeds: [embed] });
+        const topTen = message.client.currency.sort((a, b) => b.balance - a.balance)
+            .filter(user => message.client.users.cache.has(user.user_id))
+            .first(10);
+
+        if (topTen.length === 0) {
+            embed.setDescription("According to my statisticas, there is no one on the leaderboard.");
+        } else {
+            // Sequential asynchronous loop from https://advancedweb.hu/how-to-use-async-functions-with-array-foreach-in-javascript/
+            await topTen.reduce(async (memo, user, position) => {
+                await memo; // waits for previous to end
+                const userInDb = await defineUser(message.client.users.cache.get(user.user_id).id);
+                embed.addField(`${position + 1}. ${userInDb.badge || ""}${message.client.users.cache.get(user.user_id).tag}`, `${user.balance}<:ripcoin:929440348831354980>`);
+            }, undefined);
         }
 
-        await (message.client.currency.sort((a, b) => b.balance - a.balance)
-            .filter(user => message.client.users.cache.has(user.user_id))
-            .first(10)
-            .map(async (user, position) => {
-
-                const userInDb = await defineUser(message.client.users.cache.get(user.user_id).id);
-
-                console.log(userInDb.badge);
-
-                position++;
-
-                embed.addField(`${position}. ${userInDb.badge || ""}${message.client.users.cache.get(user.user_id).tag}`, `${user.balance}<:ripcoin:929440348831354980>`);
-            })
-            .join("\n") || "According to my statisticas, there is no one in the database.");
-
-        await new Promise(r => setTimeout(r, 1000)).then(async () => {
-            await sendEmbed();
-        });
-
-
+        message.channel.send({ embeds: [embed] });
     }
 };
