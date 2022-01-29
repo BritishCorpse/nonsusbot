d2de12923a17c0dd295fb51ea2ada2dbe65b409e
BritishCorpse <britishcorpse@github.noreply.com>
2022-01-29 22:33:38 +0200

    Experimenting with mentioning channels instead of giving ids.

--- command_list/counting/countingchannel.js
+++ command_list/counting/countingchannel.js
@@ -5,11 +5,25 @@ module.exports = {
     description: "Select the counting channel using this command.",
     usage: [],
     userPermissions: ["MANAGE_CHANNELS"],
-    execute(message){
+    execute(message, args){
         // Set the counting_channel_id in the serverconfig to the channel that the message was sent in.
-        message.client.serverConfig.get(message.guild.id).counting_channel_id = message.channel.id;
+        const channel = args[0].replace(/[<>@#&!]/g, "");
+
+        if (!args[0]) {
+            message.channel.send("You did not specify a channel!");
+            return;
+        }
+
+        if (isNaN(channel)) {
+            message.channel.send("Channel is not actually a channel.");
+            return;
+        }
+
+        message.client.serverConfig.get(message.guild.id).counting_channel_id = channel;
         saveServerConfig(message.client.serverConfig);
 
-        message.channel.send(`<@!${message.author.id}> the counting channel has been updated.\n\nThe counting starts at one.\nEach number is +1 from the one before it.`);
+        const countingChannel = message.client.serverConfig.get(message.guild.id).counting_channel_id;
+
+        countingChannel.send(`<@!${message.author.id}> the counting channel has been updated.\n\nThe counting starts at one.\nEach number is +1 from the one before it.`);
     }
 };
