c0777855845f958cd099e4dcbe1126af20475873
BritishCorpse <britishcorpse@github.noreply.com>
2022-03-07 22:12:53 +0200

    Issue #62 complete

---
+++ command_list/tools/contact.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "contact",
+    description: "See Graveyard's contact information.",
+    usage: [],
+    execute(message) {
+        message.reply("https://talloween.github.io/graveyardbot/contact.html");
+    }
+};
