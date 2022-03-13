d5a6355f1fe33e4307d450b048cfdafe4adfeee2
talloween <81526497+talloween@users.noreply.github.com>
2022-03-13 17:07:32 +0200

    Update m.js

--- background_tasks/m.js
+++ background_tasks/m.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: "m",
     execute (client) {
-        const m_channel_id = client.serverConfig.get(message.guild.id).m_channel_id;
-        
-        if(!m_channel_id) {return;}
-
         function deleteIfNotM(message) {
             // disable DMs
             if (message.guild === null) return;
+            
+            const m_channel_id = client.serverConfig.get(message.guild.id).m_channel_id;
+        
+            if(!m_channel_id) {return;}
 
             if (message.channel.id === m_channel_id && message.content !== "m") {
                 message.delete();
