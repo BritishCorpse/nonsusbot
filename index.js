06a3a0ada12c1e11335c7a3f5064513126d52b8c
pokir <pokir@noreply.users.github.com>
2022-03-13 16:09:55 +0300

    Remove trailing whitespaces

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const developmentConfig = require(`${__basedir}/development_config.json`);
 const client = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,   
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
         Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Discord.Intents.FLAGS.DIRECT_MESSAGES,
         Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
