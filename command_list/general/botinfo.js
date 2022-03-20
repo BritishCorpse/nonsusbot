080632a38881383c9336d8c144d3f7b5fce69d9d
talloween <talloween@github.noreply.com>
2022-03-20 15:16:16 +0200

    Added translators to the botinfo command and changed creators to developers.

--- command_list/general/botinfo.js
+++ command_list/general/botinfo.js
@@ -19,7 +19,7 @@ module.exports = {
             .setURL("https://talloween.github.io/graveyardbot/")
             .setThumbnail("https://cdn.discordapp.com/avatars/825779650822275152/3b40ed3b6e37de5018ec2bce8794ddb4.png?size=256")
             .addField("Version", require(`${__basedir}/package.json`).version)
-            .addField("Creators", `<@!${developer_discord_user_ids.join("> <@!")}>`)
+            .addField("Developers", `<@!${developer_discord_user_ids.join("> <@!")}>`)
             .addField("Artists", `<@!${artist_discord_user_ids.join("> <@!")}>`)
             .addField("Tranlators", `<@!${translator_discord_user_ids.join("> <@!")}>`)
             .addField("Made with", "[discord.js](https://discord.js.org/)")
