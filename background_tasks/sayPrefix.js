d36b88d4b089ae3d1e7349c89702b96265d1e7ac
pokir <pokir@noreply.users.github.com>
2022-04-08 18:57:17 +0300

    Fix prefix ping and ignore dms for prefix background task

--- background_tasks/sayPrefix.js
+++ background_tasks/sayPrefix.js
@@ -2,7 +2,9 @@ module.exports = {
     name: "say_prefix",
     execute (client) {
         client.on("messageCreate", message => {
-            if (message.content === "<@!" + client.user.id + ">") {
+            if (message.channel === null) return; // ignore dms
+
+            if (message.mentions.has(client.user.id, {ignoreRoles: true, ignoreEveryone: true})) {
                 message.channel.send("**My prefix here is** `" + client.serverConfig.get(message.guild.id).prefix + "`");
             }
         });
