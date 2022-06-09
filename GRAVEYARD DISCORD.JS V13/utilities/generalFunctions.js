3d204fc92402387e6865808dd38687513248b899
talloween <81526497+talloween@users.noreply.github.com>
2022-06-09 12:00:19 +0300

    Created more functions in generalFunctions.js

--- GRAVEYARD DISCORD.JS V13/utilities/generalFunctions.js
+++ GRAVEYARD DISCORD.JS V13/utilities/generalFunctions.js
@@ -1,8 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
 function formatBacktick(name) {
     return `\`\`${name}\`\``;
 }
 
+async function makeEmbed(graveyard, title, fields, color) {
+    return new MessageEmbed({
+        title: title,
+
+        fields: fields,
+
+        author: {
+            name: "Graveyard",
+            icon_url: `${graveyard.user.avatarURL()}`,
+            url: "https://talloween.github.io/graveyardbot/",
+        },
+
+        color: color,
+
+        timestamp: new Date(),
+
+        footer: {
+            text: "Powered by Mana Potions",
+        },
+    });
+}
 
 module.exports = {
-    formatBacktick
+    formatBacktick,
+    makeEmbed
 };
