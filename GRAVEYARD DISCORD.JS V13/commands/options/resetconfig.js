344db738fde0fbad3538527dc6b79b9bf9952ef5
talloween <81526497+talloween@users.noreply.github.com>
2022-06-09 12:59:16 +0300

    Fixed annoying bug;

--- GRAVEYARD DISCORD.JS V13/commands/options/resetconfig.js
+++ GRAVEYARD DISCORD.JS V13/commands/options/resetconfig.js
@@ -9,57 +9,52 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("resetconfig")
         .setDescription("Reset a configs value")
-        .addSubcommandGroup(subcommandGroup =>
-            subcommandGroup
-                .setName("reset")
-                .setDescription("Reset a config")
-                .addSubcommand(subCommand => 
-                    subCommand
-                        .setName("boolean_configs")
-                        .setDescription("Reset a true/false config")
-                        .addStringOption(option =>
-                            option
-                                .setName("config")
-                                .setDescription("The config to reset")
-                                .setRequired(true)
-                                .addChoices(
-                                    { name: "Remove bad words", value: "remove_bad_words" },
-                                    { name: "Remove all links in chat", value: "remove_links" },
-                                    { name: "Detailed logging", value: "detailed_logging" },
-                                    { name: "Remove non number messages in the counting channel", value: "remove_non_numbers_in_the_counting_channel" },
-                                    { name: "Normalize usernames that do not match the unicode standard", value: "normalize_non_unicode_names" },
-                                )
+        .addSubcommand(subCommand => 
+            subCommand
+                .setName("boolean_configs")
+                .setDescription("Reset a true/false config")
+                .addStringOption(option =>
+                    option
+                        .setName("config")
+                        .setDescription("The config to reset")
+                        .setRequired(true)
+                        .addChoices(
+                            { name: "Remove bad words", value: "remove_bad_words" },
+                            { name: "Remove all links in chat", value: "remove_links" },
+                            { name: "Detailed logging", value: "detailed_logging" },
+                            { name: "Remove non number messages in the counting channel", value: "remove_non_numbers_in_the_counting_channel" },
+                            { name: "Normalize usernames that do not match the unicode standard", value: "normalize_non_unicode_names" },
                         )
                 )
-                .addSubcommand(subCommand => 
-                    subCommand
-                        .setName("channel_configs")
-                        .setDescription("Reset a channel config")
-                        .addStringOption(option =>
-                            option
-                                .setName("config")
-                                .setDescription("The config to reset")
-                                .setRequired(true)
-                                .addChoices(
-                                    { name: "Where your will receive logged information about this server", value: "log_channel" },
-                                    { name: "Where server members will count n+1 until someone fails and the count is reset", value: "counting_channel" },
-                                    { name: "Where a message will be sent every time a user joins the server", value: "welcome_channel" },
-                                    { name: "Where a message will be sent every time a user leaves the server", value: "goodbye_channel" },
-                                )
+        )
+        .addSubcommand(subCommand => 
+            subCommand
+                .setName("channel_configs")
+                .setDescription("Reset a channel config")
+                .addStringOption(option =>
+                    option
+                        .setName("config")
+                        .setDescription("The config to reset")
+                        .setRequired(true)
+                        .addChoices(
+                            { name: "Where your will receive logged information about this server", value: "log_channel" },
+                            { name: "Where server members will count n+1 until someone fails and the count is reset", value: "counting_channel" },
+                            { name: "Where a message will be sent every time a user joins the server", value: "welcome_channel" },
+                            { name: "Where a message will be sent every time a user leaves the server", value: "goodbye_channel" },
                         )
                 )
-                .addSubcommand(subCommand => 
-                    subCommand
-                        .setName("number_configs")
-                        .setDescription("Reset a number config")
-                        .addStringOption(option =>
-                            option
-                                .setName("config")
-                                .setDescription("The config to reset")
-                                .setRequired(true)
-                                .addChoices(
-                                    { name: "Maximum word count", value: "max_word_count" },
-                                )
+        )
+        .addSubcommand(subCommand => 
+            subCommand
+                .setName("number_configs")
+                .setDescription("Reset a number config")
+                .addStringOption(option =>
+                    option
+                        .setName("config")
+                        .setDescription("The config to reset")
+                        .setRequired(true)
+                        .addChoices(
+                            { name: "Maximum word count", value: "max_word_count" },
                         )
                 )
         ),
