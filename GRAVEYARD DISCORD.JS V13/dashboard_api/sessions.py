d2d80628c6a0ca827bff8f31b1cd9b97348c1c77
pokir <pokir@users.noreply.github.com>
2022-06-15 20:34:56 +0300

    Make sure UUID is unique

--- GRAVEYARD DISCORD.JS V13/dashboard_api/sessions.py
+++ GRAVEYARD DISCORD.JS V13/dashboard_api/sessions.py
@@ -4,6 +4,9 @@ import json
 import os
 import uuid
 
+
+# TODO: deal with creating the file or fixing it if it contains invalid JSON
+
 class Sessions:
     def __init__(self, file_path):
         self.file_path = file_path
@@ -13,7 +16,13 @@ class Sessions:
 
     def create_session(self, data):
         # creates a session and returns the session id
-        session_id = str(uuid.UUID(bytes=os.urandom(16)))
+
+        while True:
+            # loop until the session id is unique (probably only runs once)
+            session_id = str(uuid.UUID(bytes=os.urandom(16)))
+
+            if session_id not in self.sessions:
+                break
 
         self.sessions[session_id] = data
 
@@ -26,7 +35,7 @@ class Sessions:
             return self.sessions[session_id]
 
         return None
-    
+
     def _read_from_file(self):
         with open(self.file_path, 'r') as f:
             self.sessions = json.load(f)
