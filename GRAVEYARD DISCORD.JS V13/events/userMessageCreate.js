282130d824d3c4bed54c7366a07f278590c672f5
halloween <talloween@github.noreply.com>
2022-06-16 21:32:42 +0300

    changed messageCreate to userMessageCreate to fix a bug

---
+++ GRAVEYARD DISCORD.JS V13/events/userMessageCreate.js
@@ -0,0 +1,18 @@
+const { userCurrency } = require("../db_objects"); 
+
+module.exports = {
+    name: "messageCreate",
+    execute(graveyard) {
+        graveyard.on("messageCreate", async message => {
+            //! THIS FILE ONLY HANDLES MESSAGES CREATED BY USERS.
+            if (message.author.bot) return;
+            
+            //
+            //! Currency
+            //
+
+            //* add one coin to the users balance
+            userCurrency.addBalance(message.author.id, 1);
+        });
+    }
+};
