ded8fcc49f0f453e8cf0e4a9669effb46a3a0025
pokir <pokir@users.noreply.github.com>
2022-07-31 19:19:47 +0200

    Try config system with a config key in a collection

--- processes/testConfigs/databaseSchemaTest.js
+++ processes/testConfigs/databaseSchemaTest.js
@@ -1,20 +1,45 @@
 const mongoose = require("mongoose");
 
-// TODO: make and use schemas for configs
+// TODO: move these validators into global utilities
+
+const discordMessageContent = {
+    type: String,
+    // TODO: put the "magic numbers" somewhere as constants (discord minimum and maximum message length)
+    minLength: [1, "Discord message content is too short."],
+    maxLength: [2000, "Discord message content is too long."],
+};
+
+const snowflake = {
+    type: String,
+    validate: {
+        validator: v => (/\d+/u).test(v),
+        message: props => `${props.value} is not a valid Snowflake`,
+    },
+};
 
 const databaseSchema = new mongoose.Schema({
-    guildId: { type: String, require: true, immutable: true },
-    channelId: { type: String, require: true, unique: true },
+    guildId: {
+        type: String, require: true, unique: true, immutable: true,
+    },
+
+    // the "configs" object in ANY collection will be treated as a config in the
+    // dashboard
     configs: {
-        message: {
-            // the message the bot sends to tell people to verify
+        messageContent: {
+            ...discordMessageContent,
+            require: true,
+            "default": "Please verify!",
         },
-        channel: {
-            id: {/* ... */},
+        channelId: {
+            ...snowflake,
+            // just in case two different guilds use the same verification
+            // channel (not really possible)
+            require: true,
+            unique: true,
         },
     },
 });
 
-const model = mongoose.model("Test_VerificationChannelConfig", databaseSchema);
+const model = mongoose.model("Test_VerificationConfig", databaseSchema);
 
 module.exports = model;
