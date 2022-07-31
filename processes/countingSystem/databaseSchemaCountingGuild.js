5bd82bfdf50f818c8d627098ca0735402927eac9
talloweener <talloween@noreply.github.com>
2022-07-31 15:26:29 +0300

    Fixed issue #207

--- processes/countingSystem/databaseSchemaCountingGuild.js
+++ processes/countingSystem/databaseSchemaCountingGuild.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const databaseSchema = new mongoose.Schema({
-    guildId: { type: String, require: true, immutable: true },
+    guildId: {
+        type: String, require: true, unique: true, immutable: true,
+    },
 
     correctlyCounted: { type: Number, require: true, "default": 0 },
     incorrectlyCounted: { type: Number, require: true, "default": 0 },
