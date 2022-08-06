38d949abdd8edb043fc4605f98018bdb29646c48
talloweener <talloween@github.noreply.com>
2022-08-06 12:23:23 +0300

    Removed unnecessary logging

--- processEventListeners/discordCommandCreate.js
+++ processEventListeners/discordCommandCreate.js
@@ -3,13 +3,11 @@ module.exports = {
     execute(client, globalUtilitiesFolder) {
         client.on("interactionCreate", async interaction => {
             if (interaction.isChatInputCommand() === false) {
-                console.log("not a command");
                 return;
             }
 
             const command = client.commands.get(interaction.commandName);
             if (!command) {
-                console.log("no command found");
                 return;
             }
 
