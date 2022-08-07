a3bc8f47d6ca944ee172ebce8babd1bb0a5b93fa
talloweener <talloween@github.noreply.com>
2022-08-07 12:17:15 +0300

    Update moderation system

---
+++ processes/moderationSystem/processDatabaseSchemas/guildMemberWarnings.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const databaseSchema = new mongoose.Schema({
+    guildId: {
+        type: String, require: true, immutable: true,
+    },
+
+    userId: {
+        type: String, require: true, immutable: true,
+    },
+
+    reason: {
+        type: String, require: true,
+    },
+
+    warningId: {
+        type: Number, require: true,
+    },
+});
+
+const model = mongoose.model("GuildModerationHistories", databaseSchema);
+
+module.exports = model;
