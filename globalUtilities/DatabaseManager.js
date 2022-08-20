29a7850723d5f0de39ddd97467029e6fbcd66140
talloween <talloween@github.noreply.com>
2022-08-20 12:45:18 +0300

    Added list-warn-remove subcommands to warn command. BROKEN, DONT USE YET

--- globalUtilities/DatabaseManager.js
+++ globalUtilities/DatabaseManager.js
@@ -13,8 +13,28 @@ class DatabaseManager {
         return databaseEntry;
     }
 
+    async findAll(schema, searchParams, createIfNotFound) {
+        let databaseEntry = await schema.find(
+            searchParams,
+        );
+
+        if (databaseEntry === null) {
+            if (createIfNotFound === false) return null;
+
+            databaseEntry = await schema.create(searchParams);
+        }
+
+        return databaseEntry;
+    }
+
     create(schema, params) {
         return schema.create(
+            params,
+        );
+    }
+
+    delete(schema, params) {
+        return schema.deleteOne(
             params,
         );
     }
--- processes/moderationSystem/processEvents/discordCommandCreate/warn.js
+++ processes/moderationSystem/processEvents/discordCommandCreate/warn.js
@@ -1,7 +1,10 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
-const guildMemberWarnings = require("../../processDatabaseSchemas/guildMemberWarnings");
-const guildModerationHistories = require("../../processDatabaseSchemas/guildModerationHistories");
+const guildMemberWarnings = require("../../processDatabaseSchemas/guildMemberWarnings.js");
+const guildModerationHistories = require("../../processDatabaseSchemas/guildModerationHistories.js");
+
+// holy shit thats a lot of directory changing right there
+const { info } = require("../../../../sources/colours.json");
 
 module.exports = {
     userPermissions: ["MODERATE_MEMBERS"],
@@ -9,38 +12,134 @@ module.exports = {
 
     data: new SlashCommandBuilder()
         .setName("warn")
-        .setDescription("Give a user a warn")
-        .addUserOption(option => option
-            .setName("user")
-            .setRequired(true)
-            .setDescription("The user to warn"))
-        .addStringOption(option => option
-            .setName("reason")
-            .setRequired(true)
-            .setDescription("The reason to warn this user")),
+        .setDescription("Warning commands")
 
+        .addSubcommand(command => command
+            .setName("add")
+            .setDescription("Add a warning to a user")
+            .addUserOption(option => option
+                .setName("user")
+                .setDescription("The user to add a warning to")
+                .setRequired(true))
+            .addStringOption(option => option
+                .setName("reason")
+                .setDescription("The reason to add a warning to this user")
+                .setRequired(true)),
+        )
+
+        .addSubcommand(command => command
+            .setName("list")
+            .setDescription("List a users warnings")
+            .addUserOption(option => option
+                .setName("user")
+                .setDescription("The user to list warnings of")
+                .setRequired(true)),
+        )
+
+        .addSubcommand(command => command
+            .setName("remove")
+            .setDescription("Remove a warning from a user")
+            .addUserOption(option => option
+                .setName("user")
+                .setDescription("The user to remove a warning from")
+                .setRequired(true))
+            .addNumberOption(option => option
+                .setName("id")
+                .setDescription("The id of the warning to remove")
+                .setRequired(true)),
+        ),
+
+    // eslint-disable-next-line consistent-return
     async execute({ data }) {
         const interaction = data.content;
 
-        const userToWarn = await interaction.options.getMember("user");
-        const reason = await interaction.options.getString("reason");
-
-        await interaction.editReply(`Gave ${userToWarn} a warning for \`${reason}\``);
+        const user = await interaction.options.getMember("user");
 
         const databaseManager = new data.globalUtilitiesFolder.DatabaseManager();
 
-        const guildInDb = await databaseManager.find(guildModerationHistories, {
-            guildId: interaction.guild.id,
-        }, true);
+        if (await interaction.options.getSubcommand() === "add") {
+            const reason = await interaction.options.getString("reason");
 
-        guildInDb.totalWarns += 1;
-        await guildInDb.save();
+            await interaction.editReply(`Gave ${user} a warning for \`${reason}\``);
 
-        databaseManager.create(guildMemberWarnings, {
-            guildId: interaction.guild.id,
-            userId: await interaction.options.getUser("user").id,
-            reason: await interaction.optoins.getString("reason"),
-            warningId: guildInDb.totalWarns,
-        }, true);
+            // find the guild in the database
+            const guildInDb = await databaseManager.find(guildModerationHistories, {
+                guildId: interaction.guild.id,
+            }, true);
+
+            // update the guilds total warnings
+            guildInDb.totalWarnings += 1;
+            await guildInDb.save();
+
+            // create a new warning in the database
+            await databaseManager.create(guildMemberWarnings, {
+                guildId: interaction.guild.id,
+                userId: user.id,
+                reason,
+                warningId: guildInDb.totalWarnings,
+            }, true);
+        } else if (await interaction.options.getSubcommand() === "list") {
+
+            const userWarnings = await databaseManager.findAll(guildMemberWarnings, {
+                guildId: interaction.guild.id,
+                userId: await interaction.options.getUser("user").id,
+            }, false);
+
+            const minWarningAmount = 1;
+
+            if (userWarnings === null || userWarnings.length < minWarningAmount) {
+                return interaction.editReply("This user does not have any warnings.");
+            }
+
+            const embedManager = new data.globalUtilitiesFolder.EmbedManager();
+
+            const color = 1052853;
+
+            embedManager.createEmbed(`Warnings for ${interaction.options.getUser("user").displayname}`, null, [], null, null, color, null);
+
+            let embedIndex = 0;
+
+            for (let i = 0; i < userWarnings.length; ++i) {
+                const warning = userWarnings[i];
+
+                // eslint-disable-next-line no-magic-numbers
+                if (i !== 0 && i % 10 === 0) {
+                    embedManager.createEmbed(`Warnings for ${interaction.options.getUser("user").username}`, null, [], null, null, color, null);
+
+                    embedIndex++;
+                }
+
+                embedManager.embeds[embedIndex].data.fields.push({
+                    name: `Warning #${warning.warningId}`,
+                    value: `Reason: ${warning.reason}`,
+                });
+            }
+
+            embedManager.addChannel(interaction.channel);
+
+            embedManager.sendEmbeds(embedManager.embeds);
+
+            await interaction.editReply("Here's what you requested.");
+        } else if (await interaction.options.getSubcommand() === "remove") {
+            const warningId = await interaction.options.getNumber("id");
+
+            const warning = await databaseManager.find(guildMemberWarnings, {
+                guildId: interaction.guild.id,
+                userId: user.id,
+                warningId,
+            }, false) || null;
+
+            if (warning === null) {
+                return interaction.editReply(`No warning found with the ID \`${warningId}\` for the user ${user}.`);
+            }
+
+            await databaseManager.delete(guildMemberWarnings, {
+                guildId: interaction.guild.id,
+                userId: user.id,
+                warningId,
+            });
+
+            return interaction.editReply(`Deleted warning \`${warningId}\` from ${user}.`);
+        }
     },
 };
